ed84844b2b15e585cca5f61a7d645c46d0a2dd6a
Austin-W-Music <info@austinwestphalmusic.com>
2024-12-27 14:26:27 -0500

    Update cloackroom.lua

--- police/config/cloackroom.lua
+++ police/config/cloackroom.lua
@@ -5,7 +5,7 @@ skins = {
 	[1] = {
 	    {name="Police Officer", model="s_m_y_cop_01"},
 	    {name="IAA Officer", model="s_m_m_ciasec_01"},
-	    {name="FIB Officer", model="s_m_y_swat_01"},
+	    {name="FBI Officer", model="s_m_y_swat_01"},
 	    {name="Undercover", model="a_m_y_genstreet_01"}
 	 },
 	[2] = {
